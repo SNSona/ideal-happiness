ce74e46406e4cb04fa3566d46e7240c1917d639e
Sona Sargsyan <sonasargsyan.ios@gmail.com>
2022-06-07 21:15:09 +0400

    Initial Commit

---
+++ Shared/CollageGlowingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollageGlowingApp.swift
+//  Shared
+//
+//  Created by Sona Sargsyan on 07.06.22.
+//
+
+import SwiftUI
+
+@main
+struct CollageGlowingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
